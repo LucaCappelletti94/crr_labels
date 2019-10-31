451482d58c5f199eac2c031ea1ce8d45078ad842
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-10-31 21:09:21 +0100

    Extended testing

--- tests/test_invalid_window_size.py
+++ tests/test_invalid_window_size.py
@@ -4,6 +4,10 @@ import pytest
 
 def test_invalid_window_size():
     with pytest.raises(ValueError):
+        fantom(["HelaS3", "GM12878"], "0")
+    with pytest.raises(ValueError):
+        fantom(["HelaS3", "GM12878"], "-1")
+    with pytest.raises(ValueError):
         fantom(["HelaS3", "GM12878"], 0)
     with pytest.raises(ValueError):
         fantom(["HelaS3", "GM12878"], -1)
