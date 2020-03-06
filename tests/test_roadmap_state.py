eb135c01755e99744d024ccffe4bdeed29097bf4
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-06 20:47:09 +0100

    Extended testing

--- tests/test_roadmap_state.py
+++ tests/test_roadmap_state.py
@@ -5,3 +5,7 @@ import pytest
 def test_roadmap_state():
     with pytest.raises(ValueError):
         roadmap(["HelaS3", "GM12878"], 200, states=10)
+        
+def test_roadmap_cell_lines():
+    with pytest.raises(ValueError):
+        roadmap(["LOTR"], 200, states=15)
